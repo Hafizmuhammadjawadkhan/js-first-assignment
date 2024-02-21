b1a7042643e00f8c172be2ba313fc438951e154a
MUHAMMAD JAWAD KHAN <khanmjawad35@gmail.com>
2024-02-21 20:14:28 +0500

    first assignmentjs first assignment

---
+++ assignment.js
@@ -0,0 +1,11 @@
+var forMeet="WELLCOME"
+var error="Error! Please enter a valid password"
+var codLand="Wellcome to js Land.....Happy Coding!"
+var land="Wellcome to js Land....."
+ var coding="Happy Coding!"
+ 
+alert("WELLCOME")
+alert(error)
+alert(codLand)
+alert(land)
+alert(coding)
